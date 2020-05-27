21a526ff4255e00e860c7b95fe494333d9e81fb8
Matt A. Tobin <email@mattatobin.com>
2020-05-26 22:18:03 -0400

    Fold global functions back into index.php

--- index.php
+++ index.php
@@ -227,7 +227,338 @@ const LICENSES = array(
 
 // ====================================================================================================================
 
-require_once('./globalFunctions.php');
+// == | Global Functions | ============================================================================================
+
+/**********************************************************************************************************************
+* Basic XML Generation
+***********************************************************************************************************************/
+function gfGenXML($aContent) {
+  // Send XML Header
+  header('Content-Type: text/xml', false);
+
+  // Write out the XML
+  print(XML_TAG . $aContent);
+
+  // We're done here
+  exit();
+}
+
+/**********************************************************************************************************************
+* Basic Content Generation using the Special Component's Template
+***********************************************************************************************************************/
+function gfGenContent($aTitle, $aContent, $aTextBox = null, $aList = null, $aError = null) {
+  $templateHead = @file_get_contents('./base/skin/special/template-header.xhtml');
+  $templateFooter = @file_get_contents('./base/skin/special/template-footer.xhtml');
+
+  // Make sure the template isn't busted, if it is send a text only error as an array
+  if (!$templateHead || !$templateFooter) {
+    gfError([__FUNCTION__ . ': Special Template is busted...', $aTitle, $aContent], -1);
+  }
+
+  // Can't use both the textbox and list arguments
+  if ($aTextBox && $aList) {
+    gfError(__FUNCTION__ . ': You cannot use both textbox and list');
+  }
+
+  // Anonymous function to determin if aContent is a string-ish or not
+  $notString = function() use ($aContent) {
+    return (!is_string($aContent) && !is_int($aContent)); 
+  };
+
+  // If not a string var_export it and enable the textbox
+  if ($notString()) {
+    $aContent = json_encode($aContent, 448);
+    $aTextBox = true;
+    $aList = false;
+  }
+
+  // Use either a textbox or an unordered list
+  if ($aTextBox) {
+    // We are using the textbox so put aContent in there
+    $aContent = '<textarea style="width: 1195px; resize: none;" name="content" rows="36" readonly>' .
+                $aContent .
+                '</textarea>';
+  }
+  elseif ($aList) {
+    // We are using an unordered list so put aContent in there
+    $aContent = '<ul><li>' . $aContent . '</li><ul>';
+  }
+
+  // Set page title
+  $templateHead = str_replace('<title></title>',
+                  '<title>' . $aTitle . ' - ' . SOFTWARE_NAME . ' ' . SOFTWARE_VERSION . '</title>',
+                  $templateHead);
+
+  if (str_contains(SOFTWARE_VERSION, 'a') || str_contains(SOFTWARE_VERSION, 'b') || str_contains(SOFTWARE_VERSION, 'pre')) {
+    $templateHead = str_replace('<!-- Special -->', '<li><a href="/special/">Special</a></li>', $templateHead);
+  }
+
+  // If we are generating an error from gfError we want to clean the output buffer
+  if ($aError) {
+    ob_get_clean();
+  }
+
+  // Send an html header
+  header('Content-Type: text/html', false);
+
+  // write out the everything
+  print($templateHead . '<h2>' . $aTitle . '</h2>' . $aContent . $templateFooter);
+
+  // We're done here
+  exit();
+}
+
+/**********************************************************************************************************************
+* Error function that will display data (Error Message)
+**********************************************************************************************************************/
+function gfError($aValue, $aMode = 0) {
+  $varExport  = var_export($aValue, true);
+  $jsonEncode = json_encode($aValue, 448); // JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+  
+  $pageHeader = array(
+    'default' => 'Unable to Comply',
+    'fatal'   => 'Fatal Error',
+    'php'     => 'PHP Error',
+    'output'  => 'Output'
+  );
+
+  switch($aMode) {
+    case -1:
+      // Text only
+      header('Content-Type: text/plain', false);
+      if (is_string($aValue) || is_int($aValue)) {
+        print($aValue);
+      }
+      else {
+        print($varExport);
+      }
+      break;
+    case 1: gfGenContent($pageHeader['php'], $aValue, null, true, true);
+            break;
+    // Deprecated, use gfGenContent
+    case 98: gfGenContent($pageHeader['output'], $jsonEncode, true);
+             break;
+    // Deprecated, use gfGenContent
+    case 99: gfGenContent($pageHeader['output'], $varExport, true);
+             break;
+    default: gfGenContent($pageHeader['default'], $aValue, null, true, true);
+  }
+
+  exit();
+}
+
+/**********************************************************************************************************************
+* PHP Error Handler
+**********************************************************************************************************************/
+
+function gfErrorHandler($errno, $errstr, $errfile, $errline) {
+  $errorCodes = array(
+    E_ERROR => 'Fatal Error',
+    E_WARNING => 'Warning',
+    E_PARSE => 'Parse',
+    E_NOTICE => 'Notice',
+    E_CORE_ERROR => 'Fatal Error (Core)',
+    E_CORE_WARNING => 'Warning (Core)',
+    E_COMPILE_ERROR => 'Fatal Error (Compile)',
+    E_COMPILE_WARNING => 'Warning (Compile)',
+    E_USER_ERROR => 'Fatal Error (User Generated)',
+    E_USER_WARNING => 'Warning (User Generated)',
+    E_USER_NOTICE => 'Notice (User Generated)',
+    E_STRICT => 'Strict',
+    E_RECOVERABLE_ERROR => 'Fatal Error (Recoverable)',
+    E_DEPRECATED => 'Depercated',
+    E_USER_DEPRECATED => 'Depercated (User Generated)',
+    E_ALL => 'All',
+  );
+
+  $errorType = $errorCodes[$errno] ?? $errno;
+  $errorMessage = $errorType . ': ' . $errstr . ' in ' .
+                  str_replace(ROOT_PATH, '', $errfile) . ' on line ' . $errline;
+
+  if (error_reporting() !== 0) {
+    gfError($errorMessage, 1);
+  }
+}
+
+set_error_handler("gfErrorHandler");
+
+/**********************************************************************************************************************
+* Unified Var Checking
+*
+* @param $_type           Type of var to check
+* @param $_value          GET/PUT/SERVER/FILES/EXISTING Normal Var
+* @param $_allowFalsy     Optional - Allow falsey returns (really only works with case var)
+* @returns                Value or null
+**********************************************************************************************************************/
+function gfSuperVar($_type, $_value, $_allowFalsy = null) {
+  $finalValue = null;
+
+  switch ($_type) {
+    case 'get':
+      $finalValue = $_GET[$_value] ?? null;
+
+      if ($finalValue) {
+        $finalValue = preg_replace('/[^-a-zA-Z0-9_\-\/\{\}\@\.\%\s\,]/', '', $_GET[$_value]);
+      }
+
+      break;
+    case 'post':
+      $finalValue = $_POST[$_value] ?? null;
+      break;
+    case 'server':
+      $finalValue = $_SERVER[$_value] ?? null;
+      break;
+    case 'files':
+      $finalValue = $_FILES[$_value] ?? null;
+      if ($finalValue) {
+        if (!in_array($finalValue['error'], [UPLOAD_ERR_OK, UPLOAD_ERR_NO_FILE])) {
+          gfError('Upload of ' . $_value . ' failed with error code: ' . $finalValue['error']);
+        }
+
+        if ($finalValue['error'] == UPLOAD_ERR_NO_FILE) {
+          $finalValue = null;
+        }
+        else {
+          $finalValue['type'] = mime_content_type($finalValue['tmp_name']);
+        }
+      }
+      break;
+    case 'cookie':
+      $finalValue = $_COOKIE[$_value] ?? null;
+      break;
+    case 'var':
+      $finalValue = $_value ?? null;
+      break;
+    default:
+      gfError('Incorrect var check');
+  }
+
+  if (!$_allowFalsy && (empty($finalValue) || $finalValue === 'none' || $finalValue === '')) {
+    return null;
+  }
+
+  return $finalValue;
+}
+
+/**********************************************************************************************************************
+* Check if a module is in $arrayIncludes
+*
+* @param $_value    A module
+* @returns          true or null depending on if $_value is in $arrayIncludes
+**********************************************************************************************************************/
+function gfEnsureModule($_value) {
+  if (!array_key_exists('arrayIncludes', $GLOBALS)) {
+    gfError('$arrayIncludes is not defined');
+  }
+  
+  if (!in_array($_value, $GLOBALS['arrayIncludes'])) {
+    return null;
+  }
+  
+  return true;
+}
+
+/**********************************************************************************************************************
+* Sends HTTP Headers to client using a short name
+*
+* @param $_value    Short name of header
+**********************************************************************************************************************/
+function gfHeader($aHeader) {
+  $headers = array(
+    404             => 'HTTP/1.1 404 Not Found',
+    501             => 'HTTP/1.1 501 Not Implemented',
+    'html'          => 'Content-Type: text/html',
+    'text'          => 'Content-Type: text/plain',
+    'xml'           => 'Content-Type: text/xml',
+    'json'          => 'Content-Type: application/json',
+    'css'           => 'Content-Type: text/css',
+    'phoebus'       => 'X-Phoebus: https://github.com/Pale-Moon-Addons-Team/phoebus/',
+  );
+  
+  if (!headers_sent() && array_key_exists($aHeader, $headers)) {
+    header($headers['phoebus']);
+    
+    if (in_array($aHeader, [404, 501])) {
+      if ($GLOBALS['gaRuntime']['debugMode'] ?? null) {
+        gfError($headers[$aHeader]);
+      }
+      else {
+        header($headers[$aHeader]);
+        exit();
+      }
+    }
+
+    header($headers[$aHeader]);
+  }
+}
+
+/**********************************************************************************************************************
+* Sends HTTP Header to redirect the client to another URL
+*
+* @param $_strURL   URL to redirect to
+**********************************************************************************************************************/
+// This function sends a redirect header
+function gfRedirect($aURL) {
+	header('Location: ' . $aURL , true, 302);
+  
+  // We are done here
+  exit();
+}
+
+/**********************************************************************************************************************
+* Splits a path into an indexed array of parts
+*
+* @param $aPath   URI Path
+***********************************************************************************************************************/
+function gfSplitPath($aPath) {
+  if ($aPath == '/') {
+    return ['root'];
+  }
+
+  return array_values(array_filter(explode('/', $aPath), 'strlen'));
+}
+
+/**********************************************************************************************************************
+* Polyfills for missing/proposed functions
+* str_starts_with, str_ends_with, str_contains
+*
+* @param $haystack  string
+* @param $needle    substring
+* @returns          true if substring exists in string else false
+**********************************************************************************************************************/
+
+if (!function_exists('str_starts_with')) {
+  function str_starts_with($haystack, $needle) {
+     $length = strlen($needle);
+     return (substr($haystack, 0, $length) === $needle);
+  }
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+if (!function_exists('str_ends_with')) {
+  function str_ends_with($haystack, $needle) {
+    $length = strlen($needle);
+    if ($length == 0) {
+      return true;
+    }
+
+    return (substr($haystack, -$length) === $needle);
+  }
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+if (!function_exists('str_contains')) {
+  function str_contains($haystack, $needle) {
+    if (strpos($haystack, $needle) > -1) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
 
 // ====================================================================================================================
 
