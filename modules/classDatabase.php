a3e83d37630b54959d723d9cf2f113ba36190459
Matt A. Tobin <email@mattatobin.com>
2021-02-20 08:58:18 -0500

    Remove redundant value from mysqli_connect_errno

--- modules/classDatabase.php
+++ modules/classDatabase.php
@@ -27,7 +27,7 @@ class classDatabase {
 
     $this->connection = mysqli_connect('localhost', $arrayCreds['username'], $arrayCreds['password'], $arrayCreds['currentDB']);
 
-    if (mysqli_connect_errno($this->connection)) {
+    if (mysqli_connect_errno()) {
       gfError('SQL Connection Error: ' . mysqli_connect_errno($this->connection));
     }
 
